cba2c430ab7cbf56f29e76737095f33b6a58d505
Kinesso Kombate <kkombat@emory.edu>
2026-08-18 14:05:33 -0400

    function example

---
+++ Function example.R
@@ -0,0 +1,15 @@
+# start out with a number to test
+x <- 3
+# you'll want your function to return this number
+x^2
+
+square <- function(x) {
+	squared_val <- x^2
+	return(squared_val)
+}
+
+square_val
+# test it out
+square(x)
+square(53)
+53^2 # does this match?
